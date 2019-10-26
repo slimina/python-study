e8c7449d840017db91c8a868aece32bec678708c
slimina <tianwei7518@qq.com>
2019-10-26 15:06:51 +0800

    demo6

--- course1/demo6.py
+++ course1/demo6.py
@@ -3,9 +3,65 @@ from datetime import datetime
 # -*- coding: utf-8 -*-
 
 
+def is_leap(year):
+    if year % 400 == 0:
+        return True
+    if year % 4 == 0 and year % 100 != 0:
+        return True
+    return False
+
+
 def main():
     input_date_str = input('请输入日期(yyyy-mm-dd):')
-    input_date = datetime.strptime(input_date_str,'%Y-%m-%d')
+    input_date = datetime.strptime(input_date_str, '%Y-%m-%d')
+    year = input_date.year
+    month = input_date.month
+    day = input_date.day
+    days_by_month = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+
+    days = day
+    days += sum(days_by_month[:month-1])
+    if is_leap(year) and month > 2:
+        days += 1
+    print("这是{}年的第{}天".format(year, days))
+
+    days_by_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(year):
+        days_by_month[1] = 29
+    print("这是{}年的第{}天".format(year, sum(days_by_month[:month-1])+day))
+
+    _30_month = {4, 6, 9, 11}
+    _31_month = {1, 3, 5, 7, 8, 10, 12}
+    days = day
+    for i in range(1, month):
+        if i in _30_month:
+            days += 30
+        elif i in _31_month:
+            days += 31
+        else:
+            days += 28
+    if is_leap(year) and month > 2:
+        days += 1
+    print("这是{}年的第{}天".format(year, days))
+
+    dd = {1: 31,
+          2: 28,
+          3: 31,
+          4: 30,
+          5: 31,
+          6: 30,
+          7: 31,
+          8: 31,
+          9: 30,
+          10: 31,
+          11: 30,
+          12: 31}
+    if is_leap(year):
+        dd[2] = 29
+    days = day
+    for i in range(1, month):
+        days += dd[i]
+    print("这是{}年的第{}天".format(year, days))
 
 
 if __name__ == '__main__':
--- course1/readme.md
+++ course1/readme.md
@@ -7,7 +7,7 @@ pow(x,y)
 sqrt(x)
 fsum(list) 集合元素求和
 ```
-# list
+# 列表 list
 ```
 append(x)
 sort()
@@ -25,9 +25,36 @@ datetime.strptime()
 datetime.strftime()
 isocalendar 返回年  周数  周几
 ```
-# 元组
+# 元组 tuple
 ```
 特殊的序列，一旦创建不能被修改，表示 : (1,2,3)
 访问和list相同
 应用场景：固定数据项，函数多返回值
+创建一个元素元组 :(1,)
+元组通常用于不同类型，而列表用于相同类型
+元组表示的是结构，列表则是顺序
+```
+
+# 集合 set
+```
+包含0个或多个数据项的无序组合
+元素不重复
+set()函数用于生成集合
+通常表示成员关系，元素去重
+s - t 或 s.difference(t) 返回集合s 不在集合t中
+s & t 或 s.intersection(t) 同时在s,t集合中
+s | t 或 s.union(t) s和 t 所有元素
+s ^ t 或 s.symmetric_difference(t)  不同时在集合s和 t中元素
+```
+
+# 字典 dict
+```
+key:value  map
+d[key] = value 增加
+d[key] 访问
+del d[key] 删除
+key in d key是否在字典中
+for key in d.keys(): 遍历key
+for value in d.values(): 遍历value
+for item in d.items(): 遍历项
 ```
